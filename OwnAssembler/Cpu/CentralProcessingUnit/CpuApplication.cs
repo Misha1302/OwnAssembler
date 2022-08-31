453584c9f37c92a05da352a389472c620b11fd82
Михаил <77919295+Misha1302@users.noreply.github.com>
2022-08-31 22:03:35 +0300

    v0.42
    Correction of logs
    Added new code example

--- OwnAssembler/Cpu/CentralProcessingUnit/CpuApplication.cs
+++ OwnAssembler/Cpu/CentralProcessingUnit/CpuApplication.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Text.RegularExpressions;
 using Connector;
 
 namespace Cpu.CentralProcessingUnit;
@@ -96,9 +95,10 @@ public class CpuApplication
         ramLogString.Append($"{_commandIndex}".PadRight(4) + "| ");
 
         for (var i = 0; i < _cpuStack.Count; i++)
-            stackLogString.Append($"{i}: {Regex.Escape(_cpuStack[i]?.ToString() ?? string.Empty)}".PadRight(20));
+            stackLogString.Append($"{i}: {ToLiteral(_cpuStack[i]?.ToString() ?? string.Empty)}".PadRight(20) + " ");
         foreach (var pair in Ram.RamDictionary)
-            stackLogString.Append($"{pair.Key}: {Regex.Escape(pair.Value?.ToString() ?? string.Empty)}".PadRight(20));
+            stackLogString.Append($"{pair.Key}: {ToLiteral(pair.Value?.ToString() ?? string.Empty)}".PadRight(20) +
+                                  " ");
 
         using (var fs = File.AppendText("stackLog.txt"))
         {
@@ -109,5 +109,62 @@ public class CpuApplication
         {
             fs.WriteLine(ramLogString);
         }
+    }
+
+    private static string ToLiteral(string input)
+    {
+        var literal = new StringBuilder(input.Length + 2);
+        literal.Append('"');
+        foreach (var c in input)
+            switch (c)
+            {
+                case '\"':
+                    literal.Append("\\\"");
+                    break;
+                case '\\':
+                    literal.Append(@"\\");
+                    break;
+                case '\0':
+                    literal.Append(@"\0");
+                    break;
+                case '\a':
+                    literal.Append(@"\a");
+                    break;
+                case '\b':
+                    literal.Append(@"\b");
+                    break;
+                case '\f':
+                    literal.Append(@"\f");
+                    break;
+                case '\n':
+                    literal.Append(@"\n");
+                    break;
+                case '\r':
+                    literal.Append(@"\r");
+                    break;
+                case '\t':
+                    literal.Append(@"\t");
+                    break;
+                case '\v':
+                    literal.Append(@"\v");
+                    break;
+                default:
+                    // ASCII printable character
+                    if (c >= 0x20 && c <= 0x7e)
+                    {
+                        literal.Append(c);
+                        // As UTF16 escaped character
+                    }
+                    else
+                    {
+                        literal.Append(@"\u");
+                        literal.Append(((int)c).ToString("x4"));
+                    }
+
+                    break;
+            }
+
+        literal.Append('"');
+        return literal.ToString();
     }
 }
